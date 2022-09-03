5e4006eaf267e088c695e2d298b45a96544b666f
Yogesh Varal <yogeshvaralhdp@gmail.com>
2022-09-03 22:31:10 +0530

    added create recipe functionality and inserted records using api

--- client/src/components/Create_Recipes.js
+++ client/src/components/Create_Recipes.js
@@ -1,11 +1,92 @@
-import React from 'react';
+import React from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import {useNavigate} from 'react-router-dom';
 
 const CreateRecipePage = () => {
-    return(
-        <div className='create-recipe-page'>
-            <h1>CreateRecipe Page</h1>
-        </div>
-    )
-}
+  const {
+    register,
+    watch,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+  const create_recipe = (data) => {
+    console.log(data);
+    const token = localStorage.getItem("REACT_TOKEN_AUTH_KEY");
+    console.log(token);
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: "Bearer " + JSON.parse(token),
+      },
+      body: JSON.stringify(data),
+    };
 
-export default CreateRecipePage
+    fetch("/recipe/recipies", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        reset()
+        navigate('/')
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="container">
+      <h1>CreateRecipe Page</h1>
+      <Form>
+        <Form.Group>
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            {...register("title", { required: true, maxLength: 25 })}
+          />
+          {errors.title && (
+            <p style={{ color: "red" }}>
+              <small>title is required</small>
+            </p>
+          )}
+          {errors.title?.type === "maxLength" && (
+            <p style={{ color: "red" }}>
+              <small>Max length is 8</small>
+            </p>
+          )}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={5}
+            {...register("description", { required: true, maxLength: 233 })}
+          />
+          {errors.description && (
+            <p style={{ color: "red" }}>
+              <small>description is required</small>
+            </p>
+          )}
+          {errors.description?.type === "maxLength" && (
+            <p style={{ color: "red" }}>
+              <small>Max length should be 255</small>
+            </p>
+          )}
+        </Form.Group>
+        <Form.Group>
+          <Button
+            as="sub"
+            variant="primary"
+            onClick={handleSubmit(create_recipe)}
+          >
+            Save
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateRecipePage;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,25 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../auth";
+import Recipe from "./Recipe";
 
 const LoggedInHome = () => {
+  const [recipies, setRecipies] = useState([]);
+  useEffect(() => {
+    fetch("/recipe/recipies")
+      .then((Response) => Response.json())
+      .then((data) => {
+        console.log(data);
+        setRecipies(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div className="recipies">
       <h1>List of Recipies</h1>
+      {recipies.map((recipe) => (
+        <Recipe title={recipe.title} description={recipe.description} />
+      ))}
     </div>
   );
 };
@@ -24,7 +38,7 @@ const HomePage = () => {
   const [logged] = useAuth();
   return (
     //  {logged?<loggedInHome/>:<loggedOutHome/>}
-    <div>{logged?<LoggedInHome/>:<LoggedOutHome/>}</div>
+    <div>{logged ? <LoggedInHome /> : <LoggedOutHome />}</div>
   );
 };
 
